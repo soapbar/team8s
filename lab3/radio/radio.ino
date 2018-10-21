2a44d31602563005b36d7c42ad2596ef08013772
erictang25 <et396@cornell.edu>
2018-10-21 16:37:21 -0400

    updated radio

--- lab3/radio/radio.ino
+++ lab3/radio/radio.ino
@@ -184,24 +184,24 @@ void loop(void)
         // Fetch the payload, and see if this was the last one.
         done = radio.read( &msg, sizeof(byte) );
 
-        boolean north;
-        boolean east;
-        boolean south;
-        boolean west;
-        char shape[];
-        char color[];
+        char north = 0;
+        char east = 0;
+        char south = 0;
+        char west = 0;
+        String shape;
+        String color;
 
         if (msg & B10000000 == B10000000) 
-          north = true;
+          north = 1;
 
         if (msg & B01000000 == B01000000) 
-          east = true;
+          east = 1;
 
         if (msg & B00100000 == B00100000) 
-          south = true;
+          south = 1;
 
         if (msg & B00010000 == B00010000) 
-          west = true;
+          west = 1;
 
         switch (msg & B00001100) {
           case B00000000:
@@ -238,8 +238,15 @@ void loop(void)
             shape = "none";
             break;
         }
-
-        Serial.println("0,0,north="+north+",east="+east+",south="+south+",west="+west+"tshape="+tshape+",tcolor="+tcolor);
+        String results = "";
+        results = results.concat(north);
+//        results = strcat(results,",east=");
+//        results = strcat(results,east);
+//        results = strcat(results,",south=");
+//        results = strcat(results,south);
+//        results = strcat(results,",west=");
+        //results = strcat(results,west);
+        Serial.println(results);
 
         // Delay just a little bit to let the other unit
         // make the transition to receiver
