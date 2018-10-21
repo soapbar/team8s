683792fb5100851c2c9d09f5c40ee1b759d87f84
erictang25 <et396@cornell.edu>
2018-10-21 15:56:43 -0400

    start at 660 hz tone

--- lab3/lab3/lab3.ino
+++ lab3/lab3/lab3.ino
@@ -20,6 +20,7 @@ int WALLRIGHT = A3;
 int WALLFRONT = A4;
 int WALLLEFT = A5;
 
+int start = 0;
 int c = 0;
 
 int rightLED = 3;
@@ -38,6 +39,8 @@ void setup() {
   // IR Setup
   Serial.begin(9600); // use the serial port
 
+  waitForSignal();
+
   // Servo Setup
   pinMode(LED_BUILTIN, OUTPUT);
   pinMode(rightLED, OUTPUT);
@@ -52,6 +55,43 @@ void loop() {
   while (1) { // reduces jitter
     followLine();
   }
+}
+
+void waitForSignal(){
+  byte prevTIMSK0 = TIMSK0;
+  byte prevADCSRA = ADCSRA;
+  byte prevADMUX = ADMUX;
+  byte prevDIDR0 = DIDR0;
+  TIMSK0 = 0; // turn off timer0 for lower jitter
+  ADCSRA = 0xe5; // set the adc to free running mode
+  ADMUX = 0x45; // use adc0
+  DIDR0 = 0x01; // turn off the digital input for adc0
+  while(start == 0){
+    for (int i = 0 ; i < 512 ; i += 2) { // save 256 samples
+      while (!(ADCSRA & 0x10)); // wait for adc to be ready
+      ADCSRA = 0xf5; // restart adc
+      byte m = ADCL; // fetch adc data
+      byte j = ADCH;
+      int k = (j << 8) | m; // form into an int
+      k -= 0x0200; // form into a signed int
+      k <<= 6; // form into a 16b signed int
+      fft_input[i] = k; // put real data into even bins
+      fft_input[i + 1] = 0; // set odd bins to 0
+    }
+    fft_window(); // window the data for better frequency response
+    fft_reorder(); // reorder the data before doing the fft
+    fft_run(); // process the data in the fft
+    fft_mag_log(); // take the output of the fft
+    sei();
+    if (fft_log_out[5] > 180) 
+      start = 1;
+  
+  }
+  TIMSK0 = prevTIMSK0;
+  ADCSRA = prevADCSRA;
+  ADMUX = prevADMUX;
+  DIDR0 = prevDIDR0;
+
 }
 
 void followLine() {
@@ -119,47 +159,22 @@ void checkIR() {
   RightServo.attach(OUTRIGHT);
 }
 
-
-void figure8(){
-  if (c%8<4) {
-    sharpRight();
-  } else {
-    sharpLeft();
-  }
-  c++;
-}
-
 // directions
 void turnLeft() {
   LeftServo.write(90);
   RightServo.write(50);
 }
+
 void turnRight() {
   LeftServo.write(130);
   RightServo.write(90);
 }
-void sharpLeft() {
-  LeftServo.write(85);
-  RightServo.write(20);
-  direction = (direction-1)%4; // update direction
-  delay(300);
 
-}
-void sharpRight() {
-  LeftServo.write(160);
-  RightServo.write(95);
-  direction = (direction+1)%4; // update direction
-  delay(300);
-}
 void goStraight() {
   LeftServo.write(93);
   RightServo.write(87);
 }
-void fullStop() {
-  LeftServo.write(90);
-  RightServo.write(90);
-  while (1);
-}
+
 void checkWall(){
   boolean right = (analogRead(WALLRIGHT)+analogRead(WALLRIGHT)/2) > 100;
   boolean front = (analogRead(WALLFRONT)+analogRead(WALLFRONT)/2) > 100;
@@ -189,9 +204,30 @@ void checkWall(){
   }
 }
 
+void sharpLeft() {
+  LeftServo.write(85);
+  RightServo.write(20);
+  direction = (direction-1)%4; // update direction
+  delay(300);
+
+}
+void sharpRight() {
+  LeftServo.write(160);
+  RightServo.write(95);
+  direction = (direction+1)%4; // update direction
+  delay(300);
+}
+
 void oneEighty() {
   LeftServo.write(100);
   RightServo.write(100);
   direction = (direction+2)%4; //update direction
   delay(660);
 }
+
+void fullStop() {
+  LeftServo.write(90);
+  RightServo.write(90);
+  while (1);
+}
+
