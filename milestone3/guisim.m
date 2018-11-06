01f75b7cee1c3206619f2b283341967c15ddedba
Unknown <hxc2@cornell.edu>
2018-11-05 23:01:16 -0500

    matlab update
    not much changed

--- milestone3/guisim.m
+++ milestone3/guisim.m
@@ -97,31 +97,31 @@ while (true)
 %
 %    Must break if the maze is fully explored.
 %======================
-%     if(wall_bin(1) == 0)
-%         next_y = curr_y - 1;
-%         next_x = curr_x;
-%         if(next_y == 0)
-%             break
-%         end
-%     elseif(wall_bin(4) == 0)
-%         next_x = curr_x - 1;
-%         next_y = curr_y;
-%         if(next_x == 0)
-%             break
-%         end
-%     elseif(wall_bin(2) == 0)
-%         next_y = curr_y + 1;
-%         next_x = curr_x;
-%         if(next_y == 6)
-%             break
-%         end
-%     elseif(wall_bin(3) == 0)
-%         next_x = curr_x + 1;
-%         next_y = curr_y;
-%         if(next_x >= x_dim+1)
-%             break
-%         end
-%     end
+    if(wall_bin(1) == 0)
+        next_y = curr_y - 1;
+        next_x = curr_x;
+        if(next_y == 0)
+            break
+        end
+    elseif(wall_bin(4) == 0)
+        next_x = curr_x - 1;
+        next_y = curr_y;
+        if(next_x == 0)
+            break
+        end
+    elseif(wall_bin(2) == 0)
+        next_y = curr_y + 1;
+        next_x = curr_x;
+        if(next_y == 6)
+            break
+        end
+    elseif(wall_bin(3) == 0)
+        next_x = curr_x + 1;
+        next_y = curr_y;
+        if(next_x >= x_dim+1)
+            break
+        end
+    end
 
        
     
